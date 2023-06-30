4f2440e95f364d75a83b32a442c79571cb9a45a6
felixrueckl <131243590+felixrueckl@users.noreply.github.com>
2023-06-30 16:34:56 +0200

    Delete bonsai-shop directory

--- bonsai-shop/public/js/script.js
+++
@@ -1,4 +0,0 @@
-// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("basic-auth JS imported successfully!");
-});
