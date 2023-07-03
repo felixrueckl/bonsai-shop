51c126a7318b740ba6ee9fe96f442a7ae06c462f
felixrueckl <rue.felix@gmail.com>
2023-07-03 17:15:04 +0200

    add Product.Model, Connect ShopOverview with ProductPages

---
+++ models/Product.model.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require("mongoose");
+
+const productSchema = new Schema({
+  imgUrl: {
+    type: String,
+  },
+  name: {
+    type: String,
+  },
+  _id: {
+    type: Schema.Types.ObjectId,
+    ref: "Product",
+  },
+  price: {
+    type: Number,
+  },
+  description: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+  stock: {
+    type: Number,
+  },
+  category: {
+    type: Number,
+  },
+});
+
+module.exports = model("Product", productSchema);
